13bd26edc3a4d83d1ec6087d8d620d8b6fffcb2e
zhaozailin <895842346@qq.com>
2013-12-18 15:15:52 +0800

    加入单元测试目录

---
+++ pinche/src/test/java/cn/egoal/pinche/controller/TestMenuController.java
@@ -0,0 +1,14 @@
+package cn.egoal.pinche.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestMenuController {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ pinche/src/test/java/cn/egoal/pinche/service/TestMenuService.java
@@ -0,0 +1,14 @@
+package cn.egoal.pinche.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestMenuService {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
